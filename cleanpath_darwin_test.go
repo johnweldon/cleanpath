b850e72086f24b006940ea09bffcbc53c85ea99f
John Weldon <johnweldon4@gmail.com>
2016-06-13 19:19:55 -0700

    add test for spaces osx

--- cleanpath_darwin_test.go
+++ cleanpath_darwin_test.go
@@ -2,7 +2,8 @@ package cleanpath_test
 
 // testCase1 is the darwin (mac osx) test case using host specific path format.
 var testCase1 = map[string]string{
-	"/a/b/c:/a/b/c:/a/b/c/": "/a/b/c",
-	"../a/b:/a/b:../a/b":    "/a/b",
-	"/a:/b:/c:/b:/a/:a":     "/a:/b:/c",
+	"/a/b/c:/a/b/c:/a/b/c/":             "/a/b/c",
+	"../a/b:/a/b:../a/b":                "/a/b",
+	"/a:/b:/c:/b:/a/:a":                 "/a:/b:/c",
+	"/has/path with spaces/bin:/a/b:/a": "/has/path with spaces/bin:/a/b:/a",
 }
